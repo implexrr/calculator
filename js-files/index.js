c0a72cb4f4dda45cd830015db5f628071ab62de1
Chenhao Wang <implexrr@gmail.com>
2022-10-17 21:21:53 -0400

    Mess around with OOP stuff

--- js-files/index.js
+++ js-files/index.js
@@ -14,6 +14,50 @@ function divide (a, b) {
   return a / b;
 }
 
-function operate (operator, a, b) {
-  return operator(a,b);
+let x;
+let y;
+
+// function operate (operator, a, b) {
+//   return operator(a,b);
+// }
+
+// function Calculation(input1, input2, operator) {
+//   console.log("Calculation initialized...");
+//   this.input1 = input1;
+//   this.input2 = input2;
+//   this.operator = operator;
+// }
+
+// Calculation.prototype.calculate = function () {
+//   return this.operator(this.input1, this.input2);
+// }
+
+// class Calculation {
+//   constructor(input1, input2, operator) {
+//     console.log("Calculation initialized...");
+//     this.input1 = input1;
+//     this.input2 = input2;
+//     this.operator = operator;
+//     this.calculate = function () {
+//       return operator(input1, input2);
+//     };
+//   }
+// }
+
+class Calculation {
+  constructor(input1, input2, operator) {
+    console.log("Calculation initialized...");
+    this.input1 = input1;
+    this.input2 = input2;
+    this.operator = operator;
+  }
+  calculate () {
+    return this.operator(this.input1, this.input2);
+  };
 }
+
+
+
+const calc1 = new Calculation(1, 2, add);
+console.log(calc1);
+console.log(calc1.calculate());
