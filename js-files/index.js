6409572355f7e64fc71dd76a9d63f22c7f883568
Chenhao Wang <implexrr@gmail.com>
2022-10-18 11:52:09 -0400

    Remove user input space

--- js-files/index.js
+++ js-files/index.js
@@ -14,50 +14,23 @@ function divide (a, b) {
   return a / b;
 }
 
-let x;
-let y;
+let input1 = "";
+let input2 = "";
+let operation = "";
 
-// function operate (operator, a, b) {
-//   return operator(a,b);
-// }
+let one = document.querySelector("#one");
+let two = document.querySelector("#two");
+let three = document.querySelector("#three");
+let four = document.querySelector("#four");
+let five = document.querySelector("#five");
+let six = document.querySelector("#six");
+let seven = document.querySelector("#seven");
+let eight = document.querySelector("#eight");
+let nine = document.querySelector("#nine");
+let multi = document.querySelector("#multiply");
+let equal = document.querySelector("#equals");
 
-// function Calculation(input1, input2, operator) {
-//   console.log("Calculation initialized...");
-//   this.input1 = input1;
-//   this.input2 = input2;
-//   this.operator = operator;
-// }
-
-// Calculation.prototype.calculate = function () {
-//   return this.operator(this.input1, this.input2);
-// }
-
-// class Calculation {
-//   constructor(input1, input2, operator) {
-//     console.log("Calculation initialized...");
-//     this.input1 = input1;
-//     this.input2 = input2;
-//     this.operator = operator;
-//     this.calculate = function () {
-//       return operator(input1, input2);
-//     };
-//   }
-// }
-
-class Calculation {
-  constructor(input1, input2, operator) {
-    console.log("Calculation initialized...");
-    this.input1 = input1;
-    this.input2 = input2;
-    this.operator = operator;
-  }
-  calculate () {
-    return this.operator(this.input1, this.input2);
-  };
-}
-
-
-
-const calc1 = new Calculation(1, 2, add);
-console.log(calc1);
-console.log(calc1.calculate());
+// When user presses a number, store it in input1
+// When user presses a operator, store/replace it in operator and store input1 in input2
+  // If input2 is empty, then you're done
+  // if input2 is already filled, then replace input2 with input1 OPERATION input2
