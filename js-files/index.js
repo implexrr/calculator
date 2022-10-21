e052f2bd41a2e58cf0c8b41f054baa0a87e8244f
Chenhao Wang <implexrr@gmail.com>
2022-10-20 21:27:00 -0400

    Write record function for number buttons

--- js-files/index.js
+++ js-files/index.js
@@ -1,63 +1,4 @@
-class Calculator {
-  constructor(x, y) {
-    console.log("Calculation initialized...");
-    this.x = x;
-    this.y = y;
-    this.operator = "";
-    this.result = "";
-    this.lastButtonEqual = false;
-    this.lastOperator = "";
-    this.lastResult = "";
-    this.input1Empty = true;
-    this.input2Empty = true;
-    this.dotPresent = false;
-    
-  }
-
-  add () {
-    return (this.x + this.y);
-  }
-
-  subtract () {
-    return (this.x - this.y);
-  }
-
-  multiply () {
-    return (this.x * this.y);
-  }
-
-  divide () {
-    return (this.x / this.y);
-  }
-
-  addOperator(operator) {
-    this.operator = operator;
-  }
-
-  preformOperation () {
-    switch (this.operator) {
-      case "add": return this.add();
-      case "subtract": return this.subtract();
-      case "multiply": return this.multiply();
-      case "divide": return this.divide();
-    }
-  }
-
-  boundRecord = this.record.bind(this);
-  record (e) {
-    console.log(this);
-    if (this.input1Empty == true) {
-      this.x = e.target.textContent;
-    }
-    else {
-      console.log('asdasd');
-      // this.x = this.x.toString().concat(e.target.textContent);
-    }
-  }
-}
-
-
-
+// Assign unique variables to each button
 let one = document.querySelector("#one");
 let two = document.querySelector("#two");
 let three = document.querySelector("#three");
@@ -69,10 +10,143 @@ let eight = document.querySelector("#eight");
 let nine = document.querySelector("#nine");
 let multi = document.querySelector("#multiply");
 let equal = document.querySelector("#equals");
-let results = document.querySelector("#results")
+let results = document.querySelector("#results");
 
-let calculation = new Calculator("", "");
 
+
+
+
+
+
+
+// Template for objects of type calculator
+class Calculator {
+  constructor() {
+    // Initialize everything
+    console.log("Calculation initialized...");
+    this.input1 = "";
+    this.input2 = "";
+    this.operator = "";
+    this.result = "";
+    this.lastButtonEqual = false;
+    this.lastOperator = "";
+    this.lastResult = "";
+    this.input1DotPresent = false;
+    this.input2DotPresent = false;
+    
+  }
+
+  // Basic arithmetic functions
+  add () {
+    return (parseFloat(this.input1) + parseFloat(this.input2)).toString();
+  }
+
+  subtract () {
+    return (parseFloat(this.input1) - parseFloat(this.input2)).toString();
+  }
+
+  multiply () {
+    return (parseFloat(this.input1) * parseFloat(this.input2)).toString();
+  }
+
+  divide () {
+    return (parseFloat(his.input1) / parseFloat(this.input2)).toString();
+  }
+
+  // Change operator value
+  addOperator(operator) {
+    this.operator = operator;
+  }
+
+  // Preform operation (based on operator value)
+  preformOperation () {
+    switch (this.operator) {
+      case "add": return this.add();
+      case "subtract": return this.subtract();
+      case "multiply": return this.multiply();
+      case "divide": return this.divide();
+    }
+  }
+
+  boundRecord = this.record.bind(this); // Allows the record method to be used outside of the class, more info @https://alephnode.io/07-event-handler-binding/
+  record (e) {
+
+    // If input1 is empty
+    if (this.input1 == "") {
+      if (e.target.textContent == ".") return; // Do nothing if user input is "."
+      
+      // Write user input into input1, display input1 on results window for calculator
+      this.input1 = e.target.textContent;
+      results.textContent = this.input1;
+    }
+
+    else {
+
+      // If input1 is non-empty, but operator is empty
+      if (this.operator == "") {
+
+        // Dot handling process
+        if ((e.target.textContent == ".") && (this.input1DotPresent== true)) return;  // Do nothing if user input is "." and dot is present in input1 string already
+        if (e.target.textContent == ".") this.input1DotPresent = true;                // If dot not already present, change state of "dot present" to true
+        
+        // Record user input, extend input1 string and display on results window for calculator
+        this.input1 = this.input1.concat(e.target.textContent);
+        results.textContent = this.input1;
+      }
+      
+      // Input1, operator both nonempty, but input2 is empty
+      else if (this.input2 = "") {
+        if (e.target.textContent == ".") return; // Do nothing if user input is "."
+
+        // Record user input, extend input2 string and display on results window for calculator
+        this.input2 = e.target.textContent;
+        results.textContent = this.input2;
+      }
+
+      // Input1, operator, and input2 all nonempty
+      else {
+
+        // If last button was "equals", the operation must have completed already, so we reset everything
+        if (this.lastButtonEqual == true) {
+          if (e.target.textContent == ".") return; // Except when there's a "." -- in this case, do nothing
+
+          // Reset everything
+          this.input1 = e.target.textContent;
+          this.input2 = "";
+          this.operator = "";
+          this.result = "";
+          this.lastButtonEqual = false;
+          this.lastOperator = "";
+          this.input1DotPresent = false;
+          this.input2DotPresent = false;
+          results.textContent = this.input1;
+        }
+
+        // Last button was not equals, so we want to extend input2 string
+        else {
+
+          // Dot handling process
+          if ((e.target.textContent == ".") && (this.input2DotPresent== true)) return;  // Do nothing if user input is "." and dot is present in input2 string already
+          if (e.target.textContent == ".") this.input2DotPresent = true;                // If dot not already present, change state of "dot present" to true
+
+
+          // Write user input into input2, display input2 on results window for calculator
+          this.input2 = this.input2.toString().concat(e.target.textContent);
+          results.textContent = this.input2;
+        }
+      }
+    }
+
+    // DEBUGGING STATEMENT
+    console.log(this);
+  }
+}
+
+// Create new calculator object
+let calculation = new Calculator();
+
+
+// Add "Record" functionality from calculator class onto all buttons with class "number"
 let numbers = document.querySelectorAll('.number');
 for (let i = 0; i < numbers.length; i++) {
   numbers[i].addEventListener('click', calculation.boundRecord);
