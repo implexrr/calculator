efe27c10a3afa228df2fc4ec13d4c92ea8e99a0d
Chenhao Wang <implexrr@gmail.com>
2022-10-25 23:29:36 -0400

    merge

--- temp.js
+++
@@ -1,7 +0,0 @@
-checkDuplicateDot (e, input) {
-  if (e.target.textContent == ".") {
-    if (this[input + "DotPresent"] == true) return true;
-    else this[input + "DotPresent"] = true;
-    return false;
-  }
-}
